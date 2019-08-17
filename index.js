79852f387a93d7a6b817629fb9f6cbdc2cb44623
Lily Keeling <lily.keeling@gmail.com>
2019-08-17 09:19:16 +0100

    creating ridiculous redirect

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hola el mundo')
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body')
+
+  body.innerHTML = '<a href="http://www.bang-employed.com/#/" onmouseover="click()"><div class="container"></div></a>'
+
+
+
+
+
+
+
+
+
+
+
+
+})
